25114cbd62e5ff53f01e1053d5a461ea267ab203
buricz9 <buricz1906@gmail.com>
2025-04-30 14:05:15 +0200

    Migration + Dockerfile

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Buricz9/notification-service
+
+go 1.24.0
---
+++ migrations/0001_create_notifications_table.sql
@@ -0,0 +1,15 @@
+CREATE
+EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE notifications
+(
+    id          UUID PRIMARY KEY     DEFAULT uuid_generate_v4(),
+    user_id     UUID        NOT NULL,
+    channel     TEXT        NOT NULL, -- "email" lub "push"
+    payload     JSONB       NOT NULL, -- dowolne dane, które musimy wysłać
+    status      TEXT        NOT NULL DEFAULT 'pending',
+    retry_count INT         NOT NULL DEFAULT 0,
+    error       TEXT,                 -- wiadomość o błędzie przy nieudanej próbie
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
